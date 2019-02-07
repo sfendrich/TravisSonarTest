c816721e1bead706d6848f0b6232a1dd8ceaebfb
Sascha Fendrich <sascha.fendrich@uni-heidelberg.de>
2019-02-07 12:46:37 +0100

    Introduce an unused variable

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -6,6 +6,8 @@ package com.example.app;
  */
 public class App 
 {
+    private int unusedVariable;
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
