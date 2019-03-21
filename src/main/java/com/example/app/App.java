2c10b3e5d6c7cf0885d5c069af7596d8d5f1065a
Sascha Fendrich <sascha.fendrich@uni-heidelberg.de>
2019-03-21 13:05:49 +0100

    Add blocker issue

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -10,6 +10,13 @@ public class App
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int i = 0;
+        switch (i) {
+          case 0:
+            System.out.println( "Hello World!" );
+          case 1:
+            System.out.println( "Hello, World!" );
+            break;
+        }
     }
 }
