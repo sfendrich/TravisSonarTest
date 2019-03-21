ae4e004d1cc5a1db2a001bfbf2bf333cd84f1112
Sascha Fendrich <sascha.fendrich@uni-heidelberg.de>
2019-03-21 13:18:27 +0100

    Fix bugs

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -1,22 +1,13 @@
 package com.example.app;
 
 /**
- * Hello world!
+ * Hello, world!
  *
  */
 public class App 
 {
-    // private int unusedVariable;
-
     public static void main( String[] args )
     {
-        int i = 0;
-        switch (i) {
-          case 0:
-            System.out.println( "Hello World!" );
-          case 1:
-            System.out.println( "Hello, World!" );
-            break;
-        }
+       System.out.println( "Hello, World!" );
     }
 }
