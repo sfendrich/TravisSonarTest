9f5684714a5999f4e25a7c09ccebadfbd52d9be4
Sascha Fendrich <sascha.fendrich@uni-heidelberg.de>
2019-04-03 10:59:10 +0200

    Does this trigger a new build on travis?

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -4,11 +4,12 @@ package com.example.app;
  * Hello, world!
  *
  * Just add some comment to have a new commit.
+ * Just another comment to have a new commit.
  */
 public class App 
 {
     public static void main( String[] args )
     {
-       System.out.println( "Hello, World!" );
+        System.out.println( "Hello, World!" );
     }
 }
